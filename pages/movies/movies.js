adb79ae460bc4b291cbd3888ce00ff0ab98fd9a8
yangxinjian <835361977@qq.com>
2017-07-23 20:41:01 +0800

    request api

--- pages/movies/movies.js
+++ pages/movies/movies.js
@@ -1,3 +1,19 @@
 Page({
-    
+  onLoad: function(event){
+    wx.request({
+      url: 'https://api.douban.com/v2/movie/top250',
+      data: {},
+      method: 'GET',
+      header: {
+        "Content-Type": "application/xml"
+      },
+      success: function(res){
+        console.log(res)
+      },
+      fail: function(){
+        console.log('fail')
+      }
+    })
+  },
+
 })
