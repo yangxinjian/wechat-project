cf79dc7938fa176a87e5ea6e38f4c0497721dbc6
yangxinjian <835361977@qq.com>
2017-07-24 09:04:22 +0800

    connect

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ App({
     //全局变量
     globalData:{
         g_isPlayingMusic: false,
-        g_currentMusicPostId: null
+        g_currentMusicPostId: null,
+        doubanBase: "https://api.douban.com"
     }
 })
--- pages/movies/movies.js
+++ pages/movies/movies.js
@@ -1,19 +1,28 @@
+var app = getApp();
+
 Page({
   onLoad: function(event){
+    var inTheatersUrl = app.globalData.doubanBase + "/v2/movie/in_theaters";
+    var comingSoonUrl = app.globalData.doubanBase +  "/v2/movie/coming_soon";
+    var top250Url = app.globalData.doubanBase +  "/v2/movie/top250";
+  
+    this.getMovieListData(inTheatersUrl);
+    this.getMovieListData(comingSoonUrl);
+    this.getMovieListData(top250Url);
+  },
+  getMovieListData: function(url){
     wx.request({
-      url: 'https://api.douban.com/v2/movie/top250',
-      data: {},
+      url: url,
       method: 'GET',
       header: {
-        "Content-Type": "application/xml"
+        "Content-Type" : "application/xml"
       },
       success: function(res){
-        console.log(res)
+        console.log(res);
       },
       fail: function(){
-        console.log('fail')
+        console.log("fail");
       }
     })
-  },
-
+  }
 })
